ccb9f8104777dd6596861efd56bfa27c5b79056e
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-12-22 18:03:33 +0300

    Initial Commit

---
+++ LOTR-Converter-SwiftUI/LOTR_Converter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_Converter_SwiftUIApp.swift
+//  LOTR-Converter-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_Converter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
